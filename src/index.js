d24928dd1988b064529345d8e8082f6744347228
Nataly Lapina <lapinann08@gmail.com>
2021-09-22 16:34:22 +0300

    add func for compare files

--- src/index.js
+++ src/index.js
@@ -9,8 +9,31 @@ const readFile = (pathToFile) => {
 };
 
 const genDiff = (filepath1, filepath2) => {
-  const file1Content = readFile(filepath1);
-  const file2Content = readFile(filepath2);
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+
+  const data1Keys = Object.keys(data1);
+  const data2Keys = Object.keys(data2);
+
+  const sotredAllKeys = _.sortBy(_.union(data1Keys, data2Keys));
+
+  const diff = sotredAllKeys.reduce((acc, key) => {
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        acc += `    ${key}: ${data1[key]}\n`;
+      } else {
+        acc += `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}\n`;
+      }
+    }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      acc += `  - ${key}: ${data1[key]}\n`;
+    }
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      acc += `  + ${key}: ${data2[key]}\n`;
+    }
+    return acc;
+  }, '');
+  return `{\n${diff}}`;
 };
 
 export default genDiff;
