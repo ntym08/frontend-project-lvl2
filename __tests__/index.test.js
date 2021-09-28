d2190c783e6fb8e09c97f7bb13d979cf9275e44a
Nataly Lapina <lapinann08@gmail.com>
2021-09-28 15:57:33 +0300

    fix test and fixtures for nested structure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff of flat json file', () => {
+test('gendiff for json file', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(readFile('result'));
 });
 
-test('gendiff of flat yaml file', () => {
+test('gendiff for yaml file', () => {
   expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toBe(readFile('result'));
 });
