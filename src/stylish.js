7324e834721da97b1a082758a69d5fb32d720c68
Nataly Lapina <lapinann08@gmail.com>
2021-10-01 21:19:39 +0300

    fix formatter stylish

--- src/stylish.js
+++ src/stylish.js
@@ -1,32 +1,39 @@
+import _ from 'lodash';
+
+const indentType = ' ';
+const spacesCount = 4;
+const signUnchanged = ' ';
+const signDeleted = '-';
+const signAdded = '+';
+
+const setIndent = (depth, spaces = 2) => indentType.repeat(depth * spacesCount - spaces);
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) return value;
+  const lines = Object.entries(value).map(([key, val]) => `${setIndent(depth)}  ${key}: ${stringify(val, depth + 1)}`);
+  return ['{', ...lines, `${setIndent(depth, spacesCount)}}`].join('\n');
+};
+
 const makeStylish = (diff) => {
   const iter = (currentValue, depth) => {
-    const signUnchanged = '  ';
-    const signDeleted = '- ';
-    const signAdded = '+ ';
-    const indentType = ' ';
-    const spacesCount = 4;
-    const indentSize = depth * spacesCount;
-    const currentIndent = indentType.repeat(indentSize - 2);
-    const bracketIndent = indentType.repeat(indentSize - spacesCount);
-
     const makeString = (object) => {
       if (object.status === 'nested') {
-        return `${currentIndent}${signUnchanged}${object.key}: ${iter(object.descendants, depth + 1)}`;
+        return `${setIndent(depth)}${signUnchanged} ${object.key}: ${iter(object.descendants, depth + 1)}`;
       }
       if (object.status === 'added') {
-        return `${currentIndent}${signAdded}${object.key}: ${object.value}`;
+        return `${setIndent(depth)}${signAdded} ${object.key}: ${stringify(object.value, depth + 1)}`;
       }
       if (object.status === 'unchanged') {
-        return `${currentIndent}${signUnchanged}${object.key}: ${object.value}`;
+        return `${setIndent(depth)}${signUnchanged} ${object.key}: ${stringify(object.value, depth + 1)}`;
       }
       if (object.status === 'deleted') {
-        return `${currentIndent}${signDeleted}${object.key}: ${object.value}`;
+        return `${setIndent(depth)}${signDeleted} ${object.key}: ${stringify(object.value, depth + 1)}`;
       }
-      return `${currentIndent}${signDeleted}${object.key}: ${object.value1}\n${currentIndent}${signAdded}${object.key}: ${object.value2}`;
+      return `${setIndent(depth)}${signDeleted} ${object.key}: ${stringify(object.value1, depth + 1)}\n${setIndent(depth)}${signAdded} ${object.key}: ${stringify(object.value2, depth + 1)}`;
     };
 
     const result = currentValue.map((item) => makeString(item));
-    return ['{', ...result, `${bracketIndent}}`].join('\n');
+    return ['{', ...result, `${setIndent(depth, spacesCount)}}`].join('\n');
   };
   return iter(diff, 1);
 };
