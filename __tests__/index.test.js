e66acb6978fd701dfa47bfadd9a4e158db013011
Nataly Lapina <lapinann08@gmail.com>
2021-10-04 10:47:43 +0300

    add test for format plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff for json file', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(readFile('result'));
+test('json-stylish', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(readFile('result-stylish'));
 });
 
-test('gendiff for yaml file', () => {
-  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toBe(readFile('result'));
+test('json-plain', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(readFile('result-plain'));
+});
+
+test('yaml-stylish', () => {
+  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toBe(readFile('result-stylish'));
+});
+
+test('yaml-plain', () => {
+  expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toBe(readFile('result-plain'));
 });
